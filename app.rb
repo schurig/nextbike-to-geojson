21511489b0af0e993865a93c4f5aa15376c32ef7
Martin Schurig <martin@schurig.pw>
2015-09-18 21:37:19 +0200

    fix json layout

--- app.rb
+++ app.rb
@@ -19,64 +19,67 @@ def bike_number(place)
   value(place, 'bike_numbers').split(',')
 end
 
-items = []
+features = []
 f = File.open('nextbike.xml')
 doc = Nokogiri::XML(f)
 doc.search('//country').each do |country|
   country.search('//city').each do |city|
     city.search('//place').each do |place|
-      item = {
-        type: 'FeatureCollection',
-        features: {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [
-              value(place, 'lat'),
-              value(place, 'lng')
-            ]
-          },
-          properties: {
+      feature = {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            value(place, 'lng'),
+            value(place, 'lat')
+          ]
+        },
+        properties: {
+          uid: value(place, 'uid'),
+          is_station: station?(place) ? true : false,
+          station: {
             uid: value(place, 'uid'),
-            is_station: station?(place) ? true : false,
-            station: {
-              uid: value(place, 'uid'),
-              number: value(place, 'number'),
-              name: value(place, 'name'),
-              bikes_count: value(place, 'bikes'),
-              bike_numbers: value(place, 'bike_numbers') ? value(place, 'bike_numbers').split(',') : nil,
-            },
-            is_bike: bike?(place) ? true : false,
-            bike: { number: bike_number(place) },
-            # city: {
-            #   uid: value(city, 'uid'),
-            #   name: value(city, 'name'),
-            #   latitude: value(city, 'lat'),
-            #   longitude: value(city, 'lng'),
-            #   zoom: value(city, 'zoom')
-            # },
-            # country: {
-            #   name: value(country, 'country_name'),
-            #   code: value(country, 'country'),
-            #   domain: value(country, 'domain'),
-            #   website: value(country, 'website'),
-            #   zoom: value(country, 'zoom')
-            # },
-            # operator: {
-            #   name: value(country, 'name'),
-            #   hotline: value(country, 'hotline'),
-            #   domain: value(country, 'domain'),
-            #   website: value(country, 'website')
-            # }
-          }
+            number: value(place, 'number'),
+            name: value(place, 'name'),
+            bikes_count: value(place, 'bikes'),
+            bike_numbers: value(place, 'bike_numbers') ? value(place, 'bike_numbers').split(',') : nil,
+          },
+          is_bike: bike?(place) ? true : false,
+          bike: { number: bike_number(place) }
         }
       }
-
-      items.push(item)
+      features.push(feature)
     end
   end
 end
 
+obj = {
+  type: 'FeatureCollection',
+  features: features
+}
+
 File.open('output.geojson', 'w') do |file|
-  file.print items.to_json
+  file.print obj.to_json
 end
+
+
+# city: {
+#   uid: value(city, 'uid'),
+#   name: value(city, 'name'),
+#   latitude: value(city, 'lat'),
+#   longitude: value(city, 'lng'),
+#   zoom: value(city, 'zoom')
+# },
+# country: {
+#   name: value(country, 'country_name'),
+#   code: value(country, 'country'),
+#   domain: value(country, 'domain'),
+#   website: value(country, 'website'),
+#   zoom: value(country, 'zoom')
+# },
+# operator: {
+#   name: value(country, 'name'),
+#   hotline: value(country, 'hotline'),
+#   domain: value(country, 'domain'),
+#   website: value(country, 'website')
+# }
